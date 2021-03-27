c3fc0908336f02518a78028ce3dbdcbfc7ede348
ot0m1 <coding_addiction@yahoo.co.jp>
2021-03-27 10:42:49 +0900

    日本語化

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_07_051807) do
+ActiveRecord::Schema.define(version: 2021_03_25_135945) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
