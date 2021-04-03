b62904edd6c5e1a8e9da171263b7a2885b04b1d7
ot0m1 <coding_addiction@yahoo.co.jp>
2021-04-03 16:17:21 +0900

    一通り実装完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:postal_code, :address, :self_introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:postal_code, :address, :self_introduction])
+  end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    # @users = User.all
+    @users = User.order(:id).page(params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+end
