88dfe979a7c4ae2c67d3e4bcdba21abf34246d4a
ot0m1 <coding_addiction@yahoo.co.jp>
2021-04-19 02:36:17 +0900

    resources :users を削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get 'users/show/:id', to: 'users#show', as: 'show'
   devise_for :users, :controllers => { :registrations => 'users/registrations', :sessions => 'users/sessions', :passwords => 'users/passwords' }
   resources :books
-  resources :users
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
