9e1e00c46c26ae8f29017f3cb9d9e7d4d956eaff
ot0m1 <coding_addiction@yahoo.co.jp>
2021-04-19 02:39:38 +0900

    不要なルーティングを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'users/index'
-  get 'users/show/:id', to: 'users#show', as: 'show'
+  get 'users/show/:id', to: 'users#show'
   devise_for :users, :controllers => { :registrations => 'users/registrations', :sessions => 'users/sessions', :passwords => 'users/passwords' }
   resources :books
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
