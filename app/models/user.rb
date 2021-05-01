7d1a417e3e8064c7ef6640870843c603f9101229
ot0m1 <coding_addiction@yahoo.co.jp>
2021-05-02 01:11:30 +0900

    一通り実装した

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_one_attached :icon
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[github]
